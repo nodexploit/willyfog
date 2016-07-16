6429cf943cb3f040511d2e1b373137bb535396c0
Adrian Gonzalez <adrigonle@gmail.com>
2016-07-16 17:58:29 +0200

    Updated script

--- bootstrap/bootstrap_db.sh
+++ bootstrap/bootstrap_db.sh
@@ -2,5 +2,5 @@
 
 # Bootstrap the database with the necessary databases and schema.
 
-mysql -uroot -proot -e "CREATE DATABASE willyfog"
-mysql -uroot -proot willyfog < /home/vagrant/willyfog/projects/willyfog-api/db/schema.sql
+mysql -uroot -proot -e "CREATE DATABASE IF NOT EXISTS willyfog_db"
+mysql -uroot -proot willyfog_db < /home/vagrant/willyfog/projects/willyfog-api/db/schema.sql
